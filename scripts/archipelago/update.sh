f96d03b169c017a774adf104827f6e3e355be232
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2019-08-19 16:18:30 -0400

    Address code review
    Make single line output
    document as to be run as n as root but give modules and vendor www-data ownership
    Be polite!
    runs as:
    docker exec -ti esmero-php bash -c 'scripts/archipelago/update.sh'

--- scripts/archipelago/update.sh
+++ scripts/archipelago/update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo -e "Updating Archipelago Packages to whatever is latest in this release!\n"
-echo -e "Running as " && whoami;
+
+# Part of the Archipelago Commons Project
+# This should be run as: docker exec -ti esmero-php bash -c 'scripts/archipelago/update.sh'
+
+echo -ne "Updating Archipelago Packages to whatever is latest in this release!\n"
+echo -ne "Running as " && whoami;
 composer update strawberryfield/strawberryfield strawberryfield/format_strawberryfield strawberryfield/webform_strawberryfield archipelago/archipelago_subtheme --no-scripts;
 (($? != 0)) && { printf '%s\n' "Updating strawberryfield packages via composer failed"; exit 1; }
 drush status bootstrap | grep -q Successful;
 (($? == 0)) && { drush cr; }
-echo -e "If you see no errors, you are up to date!"
+echo -ne "Taking ownership of drupal modules for the www-data user, this can take a little while!" && chown -R www-data web/modules/* vendor/*;
+echo -e "If you see no errors, you are up to date! Done."
