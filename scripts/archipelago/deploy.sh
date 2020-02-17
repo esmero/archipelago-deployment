2b140ee0f31432e8ca95518bd26f2d307d7fbab7
Diego Pino Navarro <dpino@metro.org>
2020-02-17 15:38:13 -0500

    Update metadata displays twig templates for beta2
    Both work correctly on Mirador and have been tested. 2.1 and 3.0 API compat.
    2.1 insists in using height and width, but those are the specs, what can we do?
    
    This also updates the deploy.sh script to install the 5th Twig template and provides TXT only versions for people mix and matching and moving forward manually over a beta1 deployment

--- scripts/archipelago/deploy.sh
+++ scripts/archipelago/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo -e "Deploying Metadata Display entities or better known as Twig Templates:\n"
 
-echo -e "General HTML block\n"
+echo -e "General HTML Description block\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_01.json;
 
-echo -e  "Linked Data HTML\n"
+echo -e  "Linked Data Block HTML\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_02.json;
 
@@ -16,6 +16,10 @@ curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Co
 echo -e "Basic MODS 3.6 Caster\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_04.json;
+
+echo -e "IIIF 2.1 Manifest\n"
+
+curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_05.json;
 
 echo -e "Last pass, setting web owner to tmp files folder\n"
 
