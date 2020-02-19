b1127f9b59662e25e103853ef46e1f5db969e73c
Diego Pino Navarro <dpino@metro.org>
2020-02-19 12:55:44 -0500

    Update deploy.sh labels to match use intend

--- scripts/archipelago/deploy.sh
+++ scripts/archipelago/deploy.sh
@@ -9,7 +9,7 @@ echo -e  "Linked Data Block HTML\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_02.json;
 
-echo -e "IIIF 3.0 Manifest for books\n"
+echo -e "IIIF 3.0 Manifest for Mirador\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_03.json;
 
@@ -17,7 +17,7 @@ echo -e "Basic MODS 3.6 Caster\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_04.json;
 
-echo -e "IIIF 2.1 Manifest\n"
+echo -e "IIIF 2.1 Manifest for Books\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPOST http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity --data-binary @d8content/metadatadisplay_entity_05.json;
 
