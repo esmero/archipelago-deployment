38d350f6ac2913f10d85b9f5359ce73b6bff0d9b
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2021-05-21 17:08:14 -0400

    Update setup.php to deal with sites/default permissions
    in Linux

--- scripts/archipelago/setup.sh
+++ scripts/archipelago/setup.sh
@@ -15,7 +15,9 @@ if (PHP_SAPI !== 'cli') {
   \$settings['reverse_proxy'] = FALSE;
 }
 EOT
+echo "Updating your web root folder permissions."
 chmod 0666 /var/www/html/web/sites/default/settings.php
+chown -R www-data:www-data /var/www/html/web/sites
 echo "Downloading JQUERY Slider Pips Library for facets" 
 mkdir -p /var/www/html/web/libraries/jquery-ui-slider-pips/dist
 curl -o /var/www/html/web/libraries/jquery-ui-slider-pips/dist/jquery-ui-slider-pips.min.js 'https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js' 
