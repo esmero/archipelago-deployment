4218521466a78f8c53ebc0dc0de98ba37c87cf20
Allison Sherrick <67483756+alliomeria@users.noreply.github.com>
2024-06-24 16:06:13 -0400

    Update update_deployed.sh
    This one too :)

--- scripts/archipelago/update_deployed.sh
+++ scripts/archipelago/update_deployed.sh
@@ -65,6 +65,14 @@ echo -e "IIIF 3.0 Thumbnail Manifest Snippet for Collections\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity/adf12524-11ca-4925-bc23-d028556e75ac --data-binary @d8content/metadatadisplay_entity_16.json;
 
+echo -e "OAI-PMH Wrapper\n"
+
+curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity/7ed7f7b2-7690-402f-93ea-a9b50a094f30 --data-binary @d8content/metadatadisplay_entity_17.json;
+
+echo -e "OAI-PMH Item with DC\n"
+
+curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/metadatadisplay_entity/metadatadisplay_entity/b5c12175-e0a9-49e9-a698-cbba342667e0 --data-binary @d8content/metadatadisplay_entity_18.json;
+
 echo -e "Updating Menu Items\n"
 
 curl -w "\n" --user jsonapi:jsonapi -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -XPATCH http://esmero-web/jsonapi/menu_link_content/menu_link_content/a580dad6-5932-4f9d-bdaa-06b5563c386c --data-binary @d8content/menu_link_content_01.json;
